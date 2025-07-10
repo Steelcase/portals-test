30b561bc1fddaef26bb37070b46ef83a31749e7b
Phil Kosiorowski <pkosioro@steelcase.com>
2025-07-10 14:39:54 -0400

    fixed pim integration

--- scripts/pim.js
+++ scripts/pim.js
@@ -1,22 +1,30 @@
-import algoliasearch from 'algoliasearch';
+import { algoliasearch } from '../node_modules/algoliasearch/dist/browser.min.js';
 
 /**
  * Returns an application from Algolia
  * @param {string} applicationID The ID for the application
  * @returns {object} The application
  */
-export default function getApplication(applicationID) {
+async function getApplication(applicationID) {
   const client = algoliasearch('BCXURW325T', 'c8de156030678c41928d24b889dcefec');
-  const index = client.initIndex('general_application_dev_amer');
+  const indexName = 'general_applications_dev_amer';
+  const response = await client.search({
+    requests: [
+      {
+        indexName,
+        query: applicationID,
+      },
+    ],
+  });
 
-  index.getObject(applicationID)
-       .then(object => {
-         console.log(object);
-         return object;
-       })
-       .catch(err => {
-         console.error(err);
-       });
-
-  return client;
+  if (response.results && response.results.length) {
+    const results = Array.from(response.results);
+    if (results[0].hits && results[0].hits.length) {
+      const hits = Array.from(results[0].hits);
+      return hits[0];
+    }
+  }
+  return null;
 }
+
+export { getApplication };
